da22f1367069a5ceecc5a43c359acb72c0afd8c8
Ugo Scarpellini <ugo.scarpellini@studio.unibo.it>
2020-09-26 11:16:16 +0200

    Modifica app.py e installazione altri pacchetti

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("hello world")
+#----------------------------Main - Chat App----------------------------------#
+# Author:       Magister17
+# Version:      v1.0
+#
+# GitHub URL:   https://github.com/Magister17/chat-app-17
+#-----------------------------------------------------------------------------#
+
+# import section
+import os                       # utilizzata per interagire con sistema operativo
+import time
+from flask_socketio import SocketIO
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
