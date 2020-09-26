9a82f2fce9fa8a89bae9ddaa995d32262c2cffdb
Ugo Scarpellini <ugo.scarpellini@studio.unibo.it>
2020-09-26 11:22:49 +0200

    Installazione ngrok

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 #----------------------------Main - Chat App----------------------------------#
 # Author:       Magister17
-# Version:      v1.0
-#
+# Version:      v1.0#
 # GitHub URL:   https://github.com/Magister17/chat-app-17
 #-----------------------------------------------------------------------------#
 
 # import section
-import os                       # utilizzata per interagire con sistema operativo
+import os
 import time
-from flask_socketio import SocketIO
 from flask import Flask, render_template
+#from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_socketio import SocketIO #, join_room, leave_room, send
 
 
 app = Flask(__name__)
